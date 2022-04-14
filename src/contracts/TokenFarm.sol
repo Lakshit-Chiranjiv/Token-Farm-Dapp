d7cbd169eb9765c34f27b0f470f859274f44ed3a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-14 11:52:41 +0530

    tokenfarm contract done till constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./DaiToken.sol";
+import "./DappToken.sol";
+
 contract TokenFarm{
     string public contractName = "Lakshit Token Farm";
+    DaiToken public daiToken;
+    DappToken public dappToken;
+
+    constructor(DaiToken _daiToken,DappToken _dappToken){
+        daiToken = _daiToken;
+        dappToken = _dappToken;
+    }
 }
