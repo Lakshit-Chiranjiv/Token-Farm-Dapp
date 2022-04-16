d6ea174f6170fb2d90078fb75956a49e2ffbf7b1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 19:51:51 +0530

    added imgs in balance component

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -5,11 +5,11 @@ const Balance = ({daiBalance=55,dappBalance=55,stakedDaiAmount=55}) => {
     <div className='mx-2'>
     <div className='d-flex justify-content-center gap-3 my-4'>
         <div className='text-center bg-dark text-white p-2 rounded w-50'>
-            <h3 className='mb-2'>DAI Balance</h3>
+            <h3 className='mb-2'>DAI Balance <span><img src="./../assets/dai.png" alt="dai" style={{width: '40px', marginLeft: '10px'}} /></span> </h3>
             <h5>{daiBalance}</h5>
         </div>
         <div className='text-center bg-dark text-white p-2 rounded w-50'>
-            <h3 className='mb-2'>DAPP Balance</h3>
+            <h3 className='mb-2'>DAPP Balance <span><img src="./../assets/token-logo.png" alt="dapp" style={{width: '40px', marginLeft: '10px'}} /></span> </h3>
             <h5>{dappBalance}</h5>
         </div>
     </div>
