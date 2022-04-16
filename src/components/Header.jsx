0742adf043ecdcd7f5801f32b0c059270ff52344
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 10:17:32 +0530

    added padding in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="bg-primary d-flex justify-content-center align-items-center gap-4">
+    <div className="bg-primary d-flex justify-content-center align-items-center gap-4 py-2">
         <img src="./../assets/tokenfarm.png" alt="farmer"  style={{width: '125px'}}/>
         <p className='display-3 text-white'>Dapp Token Farm</p>
     </div>
