a6d36fdf1927355276b71df02ee4ff8e05a0ae77
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 10:43:19 +0530

    Added the balance component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import AddressBar from './components/AddressBar';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <div className="container App">
       <Header/>
       <AddressBar/>
+      <Balance/>
     </div>
   );
 }
