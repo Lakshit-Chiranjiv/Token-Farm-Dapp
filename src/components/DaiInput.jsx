4c236d6e6ea24b4a0ca60dd9d6023f872bd07055
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 20:13:23 +0530

    added btns to stake and unstake all

--- src/components/DaiInput.jsx
+++ src/components/DaiInput.jsx
@@ -8,8 +8,14 @@ const DaiInput = () => {
             <span class="input-group-text" id="basic-addon1"><img src="./../assets/dai.png" alt="dai" className='w-50' /></span>
         </div>
         <div className="d-grid col-6 mx-auto gap-2">
-            <button className="btn btn-success">Stake</button>
-            <button className="btn btn-danger">Unstake</button>
+          <div className="d-flex gap-2">
+            <button className="btn btn-success col-6">Stake</button>
+            <button className="btn btn-danger col-6">Unstake</button>
+          </div>
+          <div className="d-flex gap-2">
+            <button className="btn btn-outline-success col-6">Stake All</button>
+            <button className="btn btn-outline-danger col-6">Unstake All</button>
+          </div>
         </div>
     </>
   )
