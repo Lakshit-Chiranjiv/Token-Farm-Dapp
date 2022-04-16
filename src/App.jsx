2d91d3281b822ab998552954e5be4ae4a6a08bdb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 20:42:19 +0530

    transaction log component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
+import React,{ useState,useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import AddressBar from './components/AddressBar';
 import Balance from './components/Balance';
 import DaiInput from './components/DaiInput';
+import TransactionLogs from './components/TransactionLogs';
 
 function App() {
+
+  const tarr = [
+    {
+      type: 'stake',
+      amount: 400
+    },
+    {
+      type: 'unstake',
+      amount: 130
+    },
+    {
+      type: 'stake',
+      amount: 90
+    },
+    {
+      type: 'earn',
+      amount: 70
+    }
+  ]
+
   return (
     <div className="container App">
       <Header/>
       <AddressBar/>
       <Balance/>
       <DaiInput/>
+      <TransactionLogs transactions={tarr.reverse()}/>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit ducimus cumque quibusdam vel dolorem voluptas ipsam officiis laudantium dicta deserunt? Sequi tempore saepe magnam incidunt similique, unde atque quo dignissimos?</p>
     </div>
   );
