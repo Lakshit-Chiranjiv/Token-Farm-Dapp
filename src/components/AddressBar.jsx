42023002d4a9273f82d07d487f24e6018fc206cd
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 10:17:46 +0530

    created address bar component

---
+++ src/components/AddressBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddressBar = ({address}) => {
+  return (
+    <div className='my-3'>
+        <h3>User Address : {address}</h3>
+    </div>
+  )
+}
+
+export default AddressBar
