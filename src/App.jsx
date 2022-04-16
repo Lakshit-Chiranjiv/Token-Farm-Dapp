d486db4646318f19707d26193e2ba6aefe7f26af
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-16 10:18:01 +0530

    added address bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import AddressBar from './components/AddressBar';
 
 function App() {
   return (
     <div className="container App">
       <Header/>
+      <AddressBar/>
     </div>
   );
 }
