08b4d2d448816258a1f8f5b07a631459e7ff16db
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-17 20:14:47 +0530

    minor correction

--- src/components/DaiInput.jsx
+++ src/components/DaiInput.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const DaiInput = () => {
   return (
     <>
-        <div class="input-group m-2 p-4 bg-primary rounded mx-auto w-50">
+        <div className="input-group m-2 p-4 bg-primary rounded mx-auto w-50">
             <input type="number" className="form-control" placeholder="Enter DAI value to stake or unstake..." aria-label="Dai value" aria-describedby="basic-addon1"/>
-            <span class="input-group-text" id="basic-addon1"><img src="./../assets/dai.png" alt="dai" className='w-50' /></span>
+            <span className="input-group-text" id="basic-addon1"><img src="./../assets/dai.png" alt="dai" className='w-50' /></span>
         </div>
         <div className="d-grid col-6 mx-auto gap-2">
           <div className="d-flex gap-2">
