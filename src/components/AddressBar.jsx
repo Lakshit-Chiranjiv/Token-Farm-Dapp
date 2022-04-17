f699adeb3256e6b07d764d2bce565cd9c310f525
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-17 20:03:26 +0530

    added function & button to connect to metamask

--- src/components/AddressBar.jsx
+++ src/components/AddressBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Web3 from 'web3'
+
+const loadWeb3 = async() =>{
+  if(window.ethereum){
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  }
+  else if(window.web3){
+    window.web3 = new Web3(window.web3.currentProvider);
+  }
+  else{
+    window.alert("No ethereum browser detected!!Please install metamask");
+  }
+}
 
 const AddressBar = ({address}) => {
   return (
     <div className='my-3'>
         <h3>User Address : {address}</h3>
+        <div className="d-grid col-6 mx-auto">
+          <button className="btn btn-success" onClick={loadWeb3}>Connect to Metamask</button>
+        </div>
     </div>
   )
 }
