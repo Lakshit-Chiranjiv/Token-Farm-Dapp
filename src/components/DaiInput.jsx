d7d676e690e40e8a3c982e5842de4b2b9b1cf627
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-18 21:15:49 +0530

    wired daiInput state variable

--- src/components/DaiInput.jsx
+++ src/components/DaiInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const DaiInput = () => {
+const DaiInput = ({setDaiInputValue}) => {
   return (
     <>
         <div className="input-group m-2 p-4 bg-primary rounded mx-auto w-50">
-            <input type="number" className="form-control" placeholder="Enter DAI value to stake or unstake..." aria-label="Dai value" aria-describedby="basic-addon1"/>
+            <input type="number" className="form-control" placeholder="Enter DAI value to stake or unstake..." aria-label="Dai value" aria-describedby="basic-addon1" onChange={(e)=>{setDaiInputValue(e.target.value)}}/>
             <span className="input-group-text" id="basic-addon1"><img src="./../assets/dai.png" alt="dai" className='w-50' /></span>
         </div>
         <div className="d-grid col-6 mx-auto gap-2">
