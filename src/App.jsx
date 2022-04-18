cf2bb275988ebe8cf00f54a7393b5682167bc6e8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-18 21:18:21 +0530

    loaded contracts and some more data with changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,57 @@ import DaiInput from './components/DaiInput';
 import TransactionLogs from './components/TransactionLogs';
 import DaiToken from './artifacts/DaiToken.json';
 import DappToken from './artifacts/DappToken.json';
+import TokenFarm from './artifacts/TokenFarm.json';
 
 
 function App() {
 
   const loadBlockchainData = async() => {
-    const web3 = window.ethereum;
+    const web3 = new Web3(window.ethereum);
   
     // const accounts = await web3.eth.getAccounts();
-    const accounts = await web3.request({ method: 'eth_accounts' });
-    console.log(accounts);
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
     setUserWalletAddress(accounts[0]);
+    console.log(accounts);
 
-    const networkId = web3.networkVersion;
-    console.log(networkId);
+    const networkId = window.ethereum.networkVersion;
+    console.log(networkId)
 
+    const daiTokenData = DaiToken.networks[networkId];
+    if(daiTokenData){
+      const daiToken = new web3.eth.Contract(DaiToken.abi,daiTokenData.address);
+      setDaiTokenContract(daiToken);
+      const daiTokenBalance = await daiToken.methods.balanceOf(userWalletAddress).call();
+      setDaiBalance(daiTokenBalance);
+      console.log(daiTokenBalance,userWalletAddress,"Hello");
+    }
+    else{
+      window.alert("Dai token contract not yet deployed on this network");
+    }
 
+    const dappTokenData = DappToken.networks[networkId];
+    if(dappTokenData){
+      const dappToken = new web3.eth.Contract(DappToken.abi,dappTokenData.address);
+      setDappTokenContract(dappToken);
+      const dappTokenBalance = await dappToken.methods.balanceOf(userWalletAddress).call();
+      setDappBalance(dappTokenBalance);
+      console.log(dappTokenBalance);
+    }
+    else{
+      window.alert("Dapp token contract not yet deployed on this network");
+    }
+
+    
+    const tokenFarmData = TokenFarm.networks[networkId];
+    if(tokenFarmData){
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi,tokenFarmData.address);
+      setTokenFarmContract(tokenFarm);
+      const stakingBalance = await tokenFarm.methods.stakedDaiBalance(userWalletAddress).call();
+      setStakedDaiAmount(stakingBalance);
+    }
+    else{
+      window.alert("Token Farm contract not yet deployed on this network");
+    }
 
   }
 
@@ -35,7 +70,7 @@ function App() {
   const [daiBalance,setDaiBalance] = useState(0);
   const [dappBalance,setDappBalance] = useState(0);
   const [stakedDaiAmount,setStakedDaiAmount] = useState(0);
-  const [dappEarningRate,setDappEarningRate] = useState(0);
+  // const [dappEarningRate,setDappEarningRate] = useState(0);
   const [daiTokenContract,setDaiTokenContract] = useState(null);
   const [dappTokenContract,setDappTokenContract] = useState(null);
   const [tokenFarmContract,setTokenFarmContract] = useState(null);
@@ -45,6 +80,11 @@ function App() {
   useEffect(()=>{
     loadBlockchainData();
   },[])
+
+
+  const stakeDaiTokens = (amount) => {
+    
+  }
 
   const tarr = [
     {
@@ -69,8 +109,8 @@ function App() {
     <div className="container App">
       <Header/>
       <AddressBar address={userWalletAddress}/>
-      <Balance/>
-      <DaiInput/>
+      <Balance daiBalance={daiBalance} dappBalance={dappBalance} stakedDaiBalance={stakedDaiAmount}/>
+      <DaiInput setDaiInputValue={setDaiInputValue}/>
       <TransactionLogs transactions={tarr.reverse()}/>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit ducimus cumque quibusdam vel dolorem voluptas ipsam officiis laudantium dicta deserunt? Sequi tempore saepe magnam incidunt similique, unde atque quo dignissimos?</p>
     </div>
