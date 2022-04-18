b273bba30557e87e4766c82d9cba9c6de4e8c8a7
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-18 18:00:25 +0530

    added code to load web3 - the new version

--- src/components/AddressBar.jsx
+++ src/components/AddressBar.jsx
@@ -14,12 +14,22 @@ const loadWeb3 = async() =>{
   }
 }
 
+const loadWeb3new = async() => {
+  if (window.ethereum) {
+    await window.ethereum.request({method: 'eth_requestAccounts'});
+    window.web3 = new Web3(window.ethereum);
+  }
+  else{
+    window.alert("No ethereum browser detected!!Please install metamask");
+  }
+}
+
 const AddressBar = ({address}) => {
   return (
     <div className='my-3'>
         <h3>User Address : {address}</h3>
         <div className="d-grid col-6 mx-auto">
-          <button className="btn btn-success" onClick={loadWeb3}>Connect to Metamask</button>
+          <button className="btn btn-success" onClick={loadWeb3new}>Connect to Metamask</button>
         </div>
     </div>
   )
