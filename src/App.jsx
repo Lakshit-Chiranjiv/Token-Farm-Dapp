e8007823a8959754a3e97665e5f721dbabb28e9d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-21 20:35:03 +0530

    added stake func and many more fixes and tests

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,10 @@ function App() {
     // const accounts = await web3.eth.getAccounts();
     const accounts = await window.ethereum.request({ method: 'eth_accounts' });
     setUserWalletAddress(accounts[0]);
+    const userAddress = accounts[0];
     console.log(accounts);
 
+    // const networkId = await web3.eth.net.getId();
     const networkId = window.ethereum.networkVersion;
     console.log(networkId)
 
@@ -28,9 +30,11 @@ function App() {
     if(daiTokenData){
       const daiToken = new web3.eth.Contract(DaiToken.abi,daiTokenData.address);
       setDaiTokenContract(daiToken);
-      const daiTokenBalance = await daiToken.methods.balanceOf(userWalletAddress).call();
+      console.log("address = "+userWalletAddress);
+      const daiTokenBalance = await daiToken.methods.balanceOf(userAddress).call();
+      // const daiTokenBalance = await daiToken.methods.balanceOf(accounts[0]).call();
       setDaiBalance(daiTokenBalance);
-      console.log(daiTokenBalance,userWalletAddress,"Hello");
+      console.log(daiTokenBalance,"hello");
     }
     else{
       window.alert("Dai token contract not yet deployed on this network");
@@ -40,9 +44,9 @@ function App() {
     if(dappTokenData){
       const dappToken = new web3.eth.Contract(DappToken.abi,dappTokenData.address);
       setDappTokenContract(dappToken);
-      const dappTokenBalance = await dappToken.methods.balanceOf(userWalletAddress).call();
+      const dappTokenBalance = await dappToken.methods.balanceOf(userAddress).call();
       setDappBalance(dappTokenBalance);
-      console.log(dappTokenBalance);
+      // console.log(dappTokenBalance);
     }
     else{
       window.alert("Dapp token contract not yet deployed on this network");
@@ -53,16 +57,35 @@ function App() {
     if(tokenFarmData){
       const tokenFarm = new web3.eth.Contract(TokenFarm.abi,tokenFarmData.address);
       setTokenFarmContract(tokenFarm);
-      const stakingBalance = await tokenFarm.methods.stakedDaiBalance(userWalletAddress).call();
+      const stakingBalance = await tokenFarm.methods.stakedDaiBalance(userAddress).call();
       setStakedDaiAmount(stakingBalance);
+      console.log(stakingBalance);
     }
     else{
       window.alert("Token Farm contract not yet deployed on this network");
     }
 
+    setLoadingPage(false);
+
   }
 
+  const getBalances = async() => {
+    setLoadingPage(true);
+    const daiTokenBalance = await daiTokenContract.methods.balanceOf(userWalletAddress).call();
+    console.log("dai token bal "+daiTokenBalance);
+    setDaiBalance(daiTokenBalance);
+    console.log(200,"bye");
+    const dappTokenBalance = await dappTokenContract.methods.balanceOf(userWalletAddress).call();
+    console.log("dapp token bal "+dappTokenBalance);
+    setDappBalance(dappTokenBalance);
+    const stakingBalance = await tokenFarmContract.methods.stakedDaiBalance(userWalletAddress).call();
+    console.log("stake bal "+stakingBalance);
+    setStakedDaiAmount(stakingBalance);
+    setLoadingPage(false);
+    console.log("done!!")
+  }
 
+  const ethWeiFactor = 1000000000000000000;
 
   const [userWalletAddress,setUserWalletAddress] = useState('');
   const [transactionsArray,setTransactionsArray] = useState([]);
@@ -76,6 +99,7 @@ function App() {
   const [tokenFarmContract,setTokenFarmContract] = useState(null);
   // const [unstakeAll,setUnstakeAll] = useState(false);
   // const [stakeAll,setStakeAll] = useState(false);
+  const [loadingPage,setLoadingPage] = useState(true);
 
   useEffect(()=>{
     loadBlockchainData();
@@ -83,7 +107,16 @@ function App() {
 
 
   const stakeDaiTokens = (amount) => {
-    
+    setLoadingPage(true);
+    daiTokenContract.methods.approve(tokenFarmContract._address,amount).send({ from: userWalletAddress }).on('transactionHash',(hash) => {
+      tokenFarmContract.methods.stakeDaiTokens(amount).send({ from: userWalletAddress }).on('transactionHash',(hash)=> {
+        getBalances();
+        console.log(daiBalance,56);
+        setLoadingPage(false)
+
+        console.log("after stake "+daiBalance,dappBalance,stakedDaiAmount);
+      })
+    })
   }
 
   const tarr = [
@@ -105,12 +138,28 @@ function App() {
     }
   ]
 
+  const checkValues = () => {
+    console.log("wallet address = "+userWalletAddress);
+    console.log("dai balance = "+daiBalance/ethWeiFactor);
+    console.log(500/24)
+    console.log("dapp balance = "+dappBalance);
+    console.log("stake balance = "+stakedDaiAmount);
+
+  }
+
   return (
     <div className="container App">
       <Header/>
       <AddressBar address={userWalletAddress}/>
-      <Balance daiBalance={daiBalance} dappBalance={dappBalance} stakedDaiBalance={stakedDaiAmount}/>
-      <DaiInput setDaiInputValue={setDaiInputValue}/>
+      <button className='btn btn-outline-primary' onClick={checkValues}>Check values</button>
+      {
+        loadingPage ? 
+        <h1>Loading ....</h1> :
+        <>
+          <Balance daiBalance={daiBalance} dappBalance={dappBalance} stakedDaiAmount={stakedDaiAmount}/>
+        </>
+      }
+      <DaiInput daiInputValue={daiInputValue} setDaiInputValue={setDaiInputValue} stakeDaiTokens={stakeDaiTokens}/>
       <TransactionLogs transactions={tarr.reverse()}/>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit ducimus cumque quibusdam vel dolorem voluptas ipsam officiis laudantium dicta deserunt? Sequi tempore saepe magnam incidunt similique, unde atque quo dignissimos?</p>
     </div>
